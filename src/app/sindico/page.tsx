950993419264813d24a899bf127dd982b92958ee
Brayan Alexandre Ivo Garrido <alexandrebrayan000@gmail.com>
2026-08-19 16:55:31 -0300

    Fix: export default na pagina do sindico

---
+++ src/app/sindico/page.tsx
@@ -0,0 +1,8 @@
+export default function SindicoPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Painel do Síndico</h1>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+}
