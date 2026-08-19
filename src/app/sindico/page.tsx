76d52e48c96576affd2af69b4d82655ba2b6a4fc
Brayan Alexandre Ivo Garrido <alexandrebrayan000@gmail.com>
2026-08-19 17:11:34 -0300

    Fix: remocao da pagina sindico para corrigir o build

--- src/app/sindico/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SindicoPage() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Painel do Síndico</h1>
-      <p>Em desenvolvimento...</p>
-    </div>
-  );
-}
