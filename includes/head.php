9d9dc190465a50e1071ba65ece7b2ea07489cfd9
Brayan Alexandre Ivo Garrido <alexandrebrayan000@gmail.com>
2026-08-20 14:54:18 -0300

    feat: adiciona pagina de admin

--- includes/head.php
+++
@@ -1,8 +0,0 @@
-﻿<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!-- CSS Global do Tema -->
-<link rel="stylesheet" href="/CondominioTech/css/global.css">
-
-<!-- JS Global do Tema -->
-<script src="/CondominioTech/js/global.js"></script>
